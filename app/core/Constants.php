f9b826b23f3c1f07535ae5c456a1b49f6886ac95
Christian Andrew <christianandrew7449@gmail.com>
2020-05-12 13:06:05 +0700

    Update

---
+++ app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define("BASE_URL", 'http://localhost/eas/public');
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "eas");
+define("DB_USERNAME", "agenda");
+define("DB_PASSWORD", "agendaapp123");
